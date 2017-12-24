c98ef27300d13770c53bc9a6a3be17dbe8860474
Ad5001 <mail@ad5001.eu>
2017-12-24 16:55:53 +0100

    Finishing installation & readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# ODS2TTS
+A simple GUI utility to speak LibreOffice document files for Linux.    
+    
+Installs the dependencies by running the "install-deps.sh" as root (sudo)
+
+### Running
+You can run a debug version of the program by launching the "debug.sh" script (Qt5 libs required).
+
+Otherwise you can download the latest release executable in the "Releases" tab.
--- install-deps.sh
+++ install-deps.sh
@@ -3,5 +3,29 @@
 cd /tmp
 git clone https://github.com/dstosberg/odt2txt
 cd odt2txt
-sudo make install
-libttspico-utils
+make install || echo "Could not install odt2txt. Are you running this script as root?"
+if which pico2wave >/dev/null; then
+	echo "Found pico2wave..."
+else
+    echo "Installing libttspico-utils"
+    INSTALLED=false
+    if which apt >/dev/null; then
+        apt install libttspico-utils || echo "Could not install libttspico-utils. Are you running this script as root?"
+        INSTALLED=true
+    fi
+    if which yum >/dev/null; then
+        yum install libttspico-utils || echo "Could not install libttspico-utils. Are you running this script as root?"
+        INSTALLED=true
+    fi
+    if $INSTALLED; then
+        echo "Successfully installed libttspico-utils"
+    else 
+        echo "Could not find a package manager. Please install libttspico-utils manually."
+    fi
+fi
+
+if which pico2wave >/dev/null; then
+    echo "Dependencies installed!"
+else
+    echo "Dependencies installation failed"
+fi
